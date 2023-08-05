cfd354ac3d09831a1f950d10db67adccd8965ef8
Tahmid Sajin <tahmid.saj@gmail.com>
2023-08-05 16:21:57 -0400

    GET single employee

--- rest-crud-apis-db-demo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ rest-crud-apis-db-demo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -5,6 +5,7 @@ import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +27,17 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    // mapping for GET /employees/{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return theEmployee;
     }
 }
