c2b2def5958a6098bf86bdf29e586312bcd6dac9
Tahmid Sajin <tahmid.saj@gmail.com>
2023-08-05 16:27:42 -0400

    POST saving employee

--- rest-crud-apis-db-demo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ rest-crud-apis-db-demo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -4,10 +4,7 @@ import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,5 +36,17 @@ public class EmployeeRestController {
         }
 
         return theEmployee;
+    }
+
+    // add mapping for POST /employees - add new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee) {
+        // also just in case they pass an id in JSON ... set id to 0
+        // this is to force a save of new item ... instead of update
+        theEmployee.setId(0);
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
     }
 }
