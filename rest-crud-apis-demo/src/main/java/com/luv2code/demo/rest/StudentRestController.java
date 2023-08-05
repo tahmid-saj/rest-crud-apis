0069494f93f73b480fa97ed07f34c08dd0db6292
Tahmid Sajin <tahmid.saj@gmail.com>
2023-08-05 03:19:29 -0400

    Refactoring get req

--- rest-crud-apis-demo/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ rest-crud-apis-demo/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.luv2code.demo.rest;
 
 import com.luv2code.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +12,24 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    // define endpoint for "/students" - return a list of students
+    private List<Student> theStudents;
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> theStudents = new ArrayList<>();
+    // define @PostConstructor to load the student data... only once
+
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
 
         theStudents.add(new Student("S", "1"));
         theStudents.add(new Student("S", "2"));
         theStudents.add(new Student("S", "3"));
+    }
+
+    // define endpoint for "/students" - return a list of students
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        loadData();
 
         return theStudents;
     }
